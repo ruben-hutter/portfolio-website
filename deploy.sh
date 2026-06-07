49d57e95c2c599a976029ebb18e7d2ebb8440e40
Ruben Hutter <ruben.hutter@rubenhutter.ch>
2026-06-07 12:32:27 +0200

    Remove host Caddyfile from repo, simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,4 @@ git pull
 podman build -t localhost/portfolio:latest .
 systemctl --user daemon-reload
 systemctl --user restart portfolio
-
-if ! cmp -s server/Caddyfile /etc/caddy/Caddyfile; then
-	sudo caddy validate --config server/Caddyfile
-	sudo cp server/Caddyfile /etc/caddy/Caddyfile
-	sudo systemctl reload caddy
-	echo "Caddy config updated and reloaded."
-fi
-
 echo "Deployed successfully!"
